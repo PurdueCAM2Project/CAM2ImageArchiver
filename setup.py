d933197a0c7ce4c540df8cbc8c092bb836b2bcfa
Sam Yellin <syellin@purdue.edu>
2018-01-25 18:30:24 -0500

    Updated setup.py download_url and author

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
   packages = ['CAM2ImageArchiver'],
   version = '0.5',
   description = 'Network camera image retrieval and archiving.',
-  author = 'CAM2 Research Group',
+  author = 'Purdue CAM2 Research Group',
   author_email = 'cam2proj@ecn.purdue.edu',
   license='Apache License 2.0',
   url = 'https://github.com/cam2proj/CAM2ImageArchiver',
-  download_url = 'https://github.com/cam2proj/CAM2ImageArchiver/archive/0.1.tar.gz',
+  download_url = 'https://github.com/cam2proj/CAM2ImageArchiver/archive/0.5.tar.gz',
   keywords = ['computer', 'vision', 'CAM2'],
   classifiers = [
     'Development Status :: 4 - Beta',
