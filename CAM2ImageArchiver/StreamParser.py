078e4d7d56d9cfae6edda81dedbd467f7f42ec87
Jia En Chua <chuaj@purdue.edu>
2018-09-17 13:32:41 -0400

    Support for both python 2, 3

--- CAM2ImageArchiver/StreamParser.py
+++ CAM2ImageArchiver/StreamParser.py
@@ -55,8 +55,8 @@ while time.time() - t < 5:
 parser.close_stream()
 
 """
-import urllib2
-
+import requests
+from six.moves.urllib.request import urlopen
 import cv2
 import numpy as np
 
@@ -171,8 +171,8 @@ class ImageStreamParser(StreamParser):
         """
         try:
             # Download the frame data.
-            frame = urllib2.urlopen(self.url, timeout=5).read()
-        except urllib2.URLError:
+            frame = urlopen(self.url, timeout=5).read()
+        except requests.URLError:
             raise error.UnreachableCameraError
 
         # Handle the cameras that return empty content.
