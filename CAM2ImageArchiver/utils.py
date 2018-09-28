b518fe0a9ab7d23262ec4daeca659112fa6f931a
Sripathm22 <mishra60@purdue.edu>
2018-09-27 21:26:22 -0400

    fix utils

--- CAM2ImageArchiver/utils.py
+++ CAM2ImageArchiver/utils.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright 2017 Purdue University
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,14 +12,17 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-'''
+"""
 import os
 
 '''
 Description:  Checks that the given file exists. 
 '''
+
+
 def check_file_exists(file_name):
     return os.path.isfile(file_name)
+
 
 def check_result_path_writable(result_path):
     return os.access(result_path, os.W_OK)
