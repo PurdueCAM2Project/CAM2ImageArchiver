86ccba02e106222c1e4b36bc224109c83a776ef8
Sripathm22 <mishra60@purdue.edu>
2018-09-29 13:05:29 -0400

    linting fix1

--- CAM2ImageArchiver/test/test_camera.py
+++ CAM2ImageArchiver/test/test_camera.py
@@ -5,7 +5,7 @@ import shutil
 from CAM2ImageArchiver.camera import Camera, IPCamera, StreamFormat
 from CAM2ImageArchiver.CAM2ImageArchiver import CAM2ImageArchiver
 
-"""
+'''
 Copyright 2017 Purdue University
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,7 +19,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-"""
+'''
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
