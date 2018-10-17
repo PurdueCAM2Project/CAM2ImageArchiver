7ceceab1cf180355095da302de5a4b7ec829303e
LING ZHANG <junelena1313@gmail.com>
2018-10-16 20:05:49 -0400

    add readme,change python support

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from setuptools import setup
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
   name = 'CAM2ImageArchiver',
   packages = ['CAM2ImageArchiver'],
-  version = '1.0',
+  version = '2.0.2',
   description = 'Network camera image retrieval and archiving.',
+  long_description = long_description,
+  long_description_content_type='text/markdown',
   author = 'Purdue CAM2 Research Group',
   author_email = 'cam2proj@ecn.purdue.edu',
   license='Apache License 2.0',
@@ -13,9 +24,10 @@ setup(
   classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
     'License :: OSI Approved :: Apache Software License'
   ],
-  python_requires='<3', # Support for V3 has not been fully tested.
+  python_requires='>=2.7',
   install_requires=[
     'certifi',
     'chardet',
